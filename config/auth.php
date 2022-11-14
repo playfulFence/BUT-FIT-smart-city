3a0daefd1efc196109c733cb2dcd5c556e9ed458
VladMikheda <vladmikheda@gmail.com>
2022-11-14 01:33:57 +0100

    automatic system changes

--- config/auth.php
+++ config/auth.php
@@ -4,7 +4,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Authentication Defaults
+    |Authentication Defaults
     |--------------------------------------------------------------------------
     |
     | This option controls the default authentication "guard" and password
@@ -20,7 +20,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Authentication Guards
+    |Authentication Guards
     |--------------------------------------------------------------------------
     |
     | Next, you may define every authentication guard for your application.
