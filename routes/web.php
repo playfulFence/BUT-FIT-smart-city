e4a8d12965b46c9779363e5f05f3ae11714b5f8c
VladMikheda <vladmikheda@gmail.com>
2022-11-14 01:34:20 +0100

    add new routs for authentication

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\Test;
+//use App\Http\Controllers\Test;
+use App\Models\Ticket;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers;
-
+//use App\Http\Controllers\Registration as Registration;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,9 +18,29 @@ use App\Http\Controllers;
 
 Route::get('/', function () {
     return phpinfo();
+})->name("hello");
+
+Route::name('registration.')
+    ->namespace('App\Http\Controllers\Registration')
+    ->middleware('auth.is')
+    ->group(function () {
+    Route::get('/registration', 'CreateController')->name("create");
+    Route::post('/registration', 'StoreController')->name("store");
 });
 
-//Route::get('/info', 'App\\Http\\Controllers\\Test@index');
-Route::get('/index', [Test::class, 'index'])->name("index");
-Route::post('/register', [Test::class, 'register'])->name("register");
+Route::name('authentication.')
+    ->namespace('App\Http\Controllers\Authentication')
+    ->middleware('auth.is')
+    ->group(function () {
+    Route::get('/login', 'IndexController')->name("index");
+    Route::post('/login', 'CheckController')->name("check");
+    Route::get('/logout', 'LogoutController')->name("exit")->withoutMiddleware('auth.is');
+});
+
+Route::name('profile.')
+    ->namespace('App\Http\Controllers\Profile')
+    ->middleware('auth')
+    ->group(function () {
+    Route::get('/profile', 'IndexController')->name("index");
+});
 
