1f6515a37be9ac54f234f86579467828fcb0cb5c
VladMikheda <vladmikheda@gmail.com>
2022-11-17 06:32:19 +0100

    add request for ticket

---
+++ app/Http/Requests/Tickets/StoreRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Tickets;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|max:255|string',
+            'address'  => 'required|max:255|string',
+            'description' => 'required|max:10000',
+            'image.*'  => 'nullable|image|mimes:jpeg,jpg,png,bmp,gif,svg'
+        ];
+    }
+}
